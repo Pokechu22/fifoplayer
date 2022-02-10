Pokechu22/fifoplayer

commit 2ad14f1949613066383618d807a6a8e9c706a95f

Fix framebuffer updates
This is done by waiting for the draw to finish.  Otherwise, the old framebuffer briefly flashes onto the screen.

source/main.cpp
@@ -557,8 +557,6 @@ int main()
 			}
 		}
 
-		// TODO: Flush WGPipe
-
 #if ENABLE_CONSOLE!=1
 		if (fifo_data.version < 2)
 		{
@@ -578,6 +576,9 @@ int main()
 			wgPipe->U8 = GX_LOAD_BP_REG;
 			wgPipe->U32 = (BPMEM_TRIGGER_EFB_COPY << 24) | copy.Hex;
 
+			GX_Flush();
+			GX_WaitDrawDone();
+
 			// TODO: This isn't quite perfect, but it at least means that we
 			// have the right width (height might be wrong, e.g. for NES games)
 			rmode->fbWidth = cur_analyzed_frame.efb_width;
@@ -595,8 +596,8 @@ int main()
 			}
 		}
 #endif
-		VIDEO_Flush();
 		VIDEO_WaitVSync();
+		VIDEO_Flush();
 		fb ^= 1;
 
 		// TODO: Menu stuff

commit d133782c2e449c260c4bd3c3241e3e1c761e67cb

Use GX_DrawDone instead of GX_WaitDrawDone
GX_DrawDone both sends the draw-done command (0x45) and waits for the interrupt, while GX_WaitDrawDone only waits for the interrupt.  Most games send the draw-done command in the fifo stream, but some (such as the wrong-title fifolog for Star Fox Adventures) do not.  This resulted in a hang on Dolphin (though, not on real hardware, which is unexpected...

This issue was introduced in 2ad14f1949613066383618d807a6a8e9c706a95f and duplicated in 694dd4402d195cf1691e63fe6810f3e2c4f019f9.

source/main.cpp
@@ -503,7 +503,7 @@ void DrawFrame(u32 cur_frame, const FifoData& fifo_data, const std::vector<Analy
 		if (screenshot)
 		{
 			GX_Flush();
-			GX_WaitDrawDone();
+			GX_DrawDone();
 
 			GXColor color;
 			for (u32 y = 0; y < cur_analyzed_frame.efb_height; y++)
@@ -536,7 +536,7 @@ void DrawFrame(u32 cur_frame, const FifoData& fifo_data, const std::vector<Analy
 		wgPipe->U32 = (BPMEM_TRIGGER_EFB_COPY << 24) | copy.Hex;
 
 		GX_Flush();
-		GX_WaitDrawDone();
+		GX_DrawDone();
 
 		// TODO: This isn't quite perfect, but it at least means that we
 		// have the right width (height might be wrong, e.g. for NES games)